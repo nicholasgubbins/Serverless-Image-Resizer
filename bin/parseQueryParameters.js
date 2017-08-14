7c4f1cbd807588e6b16941b9c192c1b7ffd13fb3
marcusmolchany <marcusmolchany@gmail.com>
2017-08-13 23:25:36 -0400

    fix linting errors in parseQueryParameters

--- bin/parseQueryParameters.js
+++ bin/parseQueryParameters.js
@@ -1,25 +1,27 @@
-
-
-function parseQueryParameters(query) {
+function parseQueryParameters(queryArg) {
+  const query = Object.assign({}, queryArg);
   const format = {};
-  if ((query.w && parseInt(query.w) !== NaN) || (query.h && parseInt(query.h) !== NaN)) {
+  if ((query.w && !isNaN(parseInt(query.w, 10))) || (query.h && !isNaN(parseInt(query.h, 10)))) {
     format.gravity = 'Center';
     format.resize = {};
     if (query.h && query.h.length > 4) {
-      const test_height_first = query.h.slice(0, query.h.length / 2);
-      const test_height_second = query.h.slice((query.h.length / 2));
-      if (test_height_first == test_height_second) {
-        query.h = test_height_first;
+      const testHeightFirst = query.h.slice(0, query.h.length / 2);
+      const testHeightSecond = query.h.slice((query.h.length / 2));
+      if (testHeightFirst === testHeightSecond) {
+        query.h = testHeightFirst;
       }
     }
     if (query.f) format.filter = 'Point';
-    format.resize.width = (query.w && parseInt(query.w) !== NaN) ? parseInt(query.w) : null;
-    format.resize.height = (query.h && parseInt(query.h) !== NaN) ? parseInt(query.h) : null;
-    if (format.resize.height !== null && format.resize.width !== null) format.crop = { width: format.resize.width, height: format.resize.height };
+    format.resize.width = (query.w && !isNaN(parseInt(query.w, 10))) ? parseInt(query.w, 10) : null;
+    format.resize.height =
+      (query.h && !isNaN(parseInt(query.h, 10))) ? parseInt(query.h, 10) : null;
+    if (format.resize.height !== null && format.resize.width !== null) {
+      format.crop = { width: format.resize.width, height: format.resize.height };
+    }
   }
   if (query.f) format.filter = query.f;
-  if (query.q && parseInt(query.q) !== NaN) format.quality = parseInt(query.q);
-  if (query.m && parseInt(query.m) !== NaN) format.max = parseInt(query.m);
+  if (query.q && !isNaN(parseInt(query.q, 10))) format.quality = parseInt(query.q, 10);
+  if (query.m && !isNaN(parseInt(query.m, 10))) format.max = parseInt(query.m, 10);
   if (query.b && query.b.match(/[0-9]{1,}x[0-9]{1,}/g)) format.blur = query.b.split('x');
   return format;
 }
